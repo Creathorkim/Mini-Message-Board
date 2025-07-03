9440a47d15fc1c53c54da3e1dde9dbbf168f312e
Mustakeem Ololade <mustakeemololade@gmail.com>
2025-07-04 00:08:22 +0100

    Add dynamic port configuration using environment variable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT | 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
